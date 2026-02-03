33433c54a0e649bbff519a2ef7bbcbd449a52551
savioti <matheussavioti@gmail.com>
2026-02-03 10:50:45 -0300

    aula 30 terminada

--- Next.js/chronos-pomodoro/src/App.tsx
+++ Next.js/chronos-pomodoro/src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
@@ -36,10 +37,7 @@ export function App() {
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
 
-          <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0 0</p>
-          </div>
+          <Cycles />
 
           <div className="formRow">
             <button type="submit">Começar</button>
