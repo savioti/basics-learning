78ba2e80f00ec2c5d50ec1e6c721144290ba681e
Ângelo Savioti <matheussavioti@gmail.com>
2026-04-08 01:59:49 -0300

    finalizada a aula 48

--- Next.js/chronos-pomodoro/src/App.tsx
+++ Next.js/chronos-pomodoro/src/App.tsx
@@ -1,7 +1,29 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Home } from "./pages/Home";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
+import { TaskContext, TaskContextProvider } from "./contexts/TaskContext";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
